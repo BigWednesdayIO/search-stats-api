f2636bb56fdf82faeaf8846a184d60b2f629e754
Chris Impey <chris@bigwednesday.io>
2015-10-26 15:56:57 +0000

    renable tests but change dummy tests to pass

--- test/sample_tests.js
+++ test/sample_tests.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 
 describe('sample', () => {
-  it('fails', () => {
-    expect(true).to.equal(false);
+  it('passes', () => {
+    expect(true).to.equal(true);
   });
 });
